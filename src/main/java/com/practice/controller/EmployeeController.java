a22b8160af7dba0669eb8705fc94e59870a77c90
krishna teja akella <krishnatejaakella@krishnas-MacBook-Air.local>
2023-03-16 18:34:32 +0530

    fourth commit

--- src/main/java/com/practice/controller/EmployeeController.java
+++ src/main/java/com/practice/controller/EmployeeController.java
@@ -41,7 +41,7 @@ public class EmployeeController {
 		return e;
 
 	}
-//This is to fetch employeeById
+//This is to fetch employeeById testing
 	@GetMapping("/fetchByName/{id}")
 	public List<EmployeeDto> fetchEmployeeDetailsByName(@PathVariable("id") Integer id) {
 		return employeeservice.fetchEmployeeDetailsByName(id);
